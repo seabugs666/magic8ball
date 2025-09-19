8471d10bd7073b12f3485ea88f54c89083cfa8df
seabugs666 <130319679+seabugs666@users.noreply.github.com>
2025-09-19 12:44:53 -0400

    light

--- src/main.js
+++ src/main.js
@@ -51,6 +51,11 @@ renderer.domElement.addEventListener('touchstart', preventDefault, { passive: fa
 renderer.domElement.addEventListener('touchmove', preventDefault, { passive: false });
 
 // === Lights ===
+// Camera-aligned key light
+let keyLight = new THREE.DirectionalLight(0xffffff, 0.5);
+keyLight.position.set(0, 3, 1);
+scene.add(keyLight);
+
 
 // Rim / edge lights
 const rim1 = new THREE.DirectionalLight(0xffffff, 0.5);
